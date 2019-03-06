371bdfd432e0fa3e8ad3de87c519e396d15bb395
WangLei <wanglei0717@outlook.com>
2019-03-06 18:35:36 +0800

    Add 'ScoreSheet' class to store students infor to print

---
+++ src/main/java/company/model/ScoreSheet.java
@@ -0,0 +1,45 @@
+package main.java.company.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ScoreSheet {
+    private List<Student> studentList;
+    private double average;
+    private double median;
+
+    public ScoreSheet() {
+        this.studentList = new ArrayList<>();
+    }
+
+    public List<Student> getStudentList() {
+        return this.studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public double getAverage() {
+        return this.average;
+    }
+
+    public void setAverage() {
+        this.average = this.studentList.stream().collect(Collectors.averagingDouble(Student::getSum));
+    }
+
+    public double getMedian() {
+        return this.median;
+    }
+
+    public void setMedian() {
+        List<Double> sortedList = this.studentList.stream().map(Student::getSum).sorted().collect(Collectors.toList());
+        int medianIndex = sortedList.size() / 2;
+        if (0 == sortedList.size() % 2) {
+            this.median = (sortedList.get(medianIndex) + sortedList.get(medianIndex - 1)) / 2.0;
+        } else {
+            this.median = sortedList.get(medianIndex);
+        }
+    }
+}
