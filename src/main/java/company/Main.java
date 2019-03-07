57b64e4327a60e264b1275b032ebd188baa96168
WangLei <wanglei0717@outlook.com>
2019-03-07 10:09:22 +0800

    Add methods to verify input

--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -1,8 +1,22 @@
 package main.java.company;
 
+import main.java.company.service.Service;
+import main.java.company.tools.Tools;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Tools tools = new Tools();
+        Service service = new Service();
+
+        tools.printMainMenu();
+        boolean verifyResult = true;
+        do {
+            tools.promptInputStudentInfor(verifyResult);
+            String input = service.readInput();
+            verifyResult = service.verifyInput(input);
+        } while (!verifyResult);
+
+
     }
 }
--- src/main/java/company/service/Service.java
+++ src/main/java/company/service/Service.java
@@ -1,19 +1,52 @@
 package main.java.company.service;
 
+import main.java.company.tools.Tools;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Service {
+    private Tools tools = new Tools();
+
     public String readInput() {
         String input = null;
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         try {
             input = bufferedReader.readLine();
-            bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return input;
     }
+
+    public boolean verifyInput(String input) {
+        if (null == input) {
+            return false;
+        }
+        String[] inforArray = input.split("[,，]");
+        if (inforArray.length < 6) {
+            return false;
+        }
+        if (!inforArray[1].matches("^\\d*$")) {
+            return false;
+        }
+        return Arrays.stream(inforArray)
+                .skip(2)
+                .allMatch(this::verifyInputScores);
+    }
+
+    private boolean verifyInputScores(String score) {
+        String[] subjectAndScore = score.split("[:：]");
+        if (2 != subjectAndScore.length) {
+            return false;
+        }
+        if (null == subjectAndScore[0]) {
+            return false;
+        }
+        return subjectAndScore[1].matches("^\\d*$");
+    }
+
+
 }
